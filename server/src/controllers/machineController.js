bd8e9e5c9c21cab3f6a2ea86d185a8e3256e0e72
Akshay Shelke <akshaysharadshelke86@gmail.com>
2026-08-17 09:17:36 +0530

    fix: Update SET_PARAMETERS command to strictly match IoT requested format

--- server/src/controllers/machineController.js
+++ server/src/controllers/machineController.js
@@ -248,7 +248,6 @@ export const updateMachine = async (req, res) => {
       // Format A: Legacy format (Exactly what worked in the morning)
       const payloadString = [
         machine_id,
-        "SET_PARAMETERS",
         hardwareStatus,
         modeStr,
         usesAmt,
